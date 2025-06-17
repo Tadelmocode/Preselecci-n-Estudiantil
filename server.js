35643f135a94c6ba6b6a67534cc4fcfcbba11660
Tadelmocode <Jesustadelmo1700@gmail.com>
2025-06-17 15:34:12 -0400

    nuevos commit

--- server.js
+++ server.js
@@ -242,12 +242,7 @@ app.delete('/admin/delete/:id', async (req, res) => {
         res.status(500).send('Error al eliminar el registro');
     }
 });
-  
+    
+module.exports = app;
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
  
-module.exports = app; // Exporta la aplicación para uso en Vercel
-// Esto es necesario para que Vercel pueda manejar la aplicación correctamente
